ef69fc8343b429bee4cfc14b9de7377b83bc566f
Rizki M Citra <62492410+rimzzlabs@users.noreply.github.com>
2024-03-12 03:35:47 +0700

    fix(theme-switch): hydration error (#5)
    detect current env if server just return spinner

--- src/modules/dashboard/navbar/theme-switch.tsx
+++ src/modules/dashboard/navbar/theme-switch.tsx
@@ -10,9 +10,10 @@ import {
 
 import { useMediaQuery } from '#/hooks/use-media-query'
 
-import { MoonIcon, SunIcon } from 'lucide-react'
+import { Loader2Icon, MoonIcon, SunIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
-import { match } from 'ts-pattern'
+import { useEffect, useState } from 'react'
+import { P, match } from 'ts-pattern'
 
 const dropdownItems = [
   { text: 'Terang', value: 'light' },
@@ -23,18 +24,28 @@ const dropdownItems = [
 export function DashboardNavbarThemeSwitch() {
   const { setTheme, resolvedTheme } = useTheme()
   const updateTheme = (newTheme: string) => setTheme(newTheme)
+  const [isClient, setIsClient] = useState(false)
+
   const isMobile = useMediaQuery('only screen and (max-width: 768px)')
 
-  const icon = match(resolvedTheme)
-    .with('dark', () => <MoonIcon size='1rem' />)
-    .otherwise(() => <SunIcon size='1rem' />)
+  const icon = match(isClient)
+    .with(true, () =>
+      match(resolvedTheme)
+        .with(P.string.includes('light'), () => <SunIcon size='1rem' />)
+        .otherwise(() => <MoonIcon size='1rem' />),
+    )
+    .otherwise(() => <Loader2Icon size='1rem' className='animate-spin' />)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
   if (isMobile) return null
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button size='icon' variant='ghost'>
+        <Button size='icon' variant='ghost' disabled={!isClient}>
           {icon}
           <span className='sr-only'>Pilih Tema</span>
         </Button>
